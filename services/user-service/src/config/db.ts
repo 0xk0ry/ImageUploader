847e2b87fba935f33ebbbb8fe1bf2f274d40001f
kory <phamtruc.main@gmail.com>
2026-02-09 22:01:50 +0700

    refactor(post-service, user-service): update PrismaClient import path and remove output configuration

--- services/user-service/src/config/db.ts
+++ services/user-service/src/config/db.ts
@@ -1,6 +1,6 @@
 import { Pool } from 'pg';
 import { PrismaPg } from '@prisma/adapter-pg';
-import { PrismaClient } from '../generated/index.js';
+import { PrismaClient } from '@prisma/client';
 
 const connectionString = process.env.DATABASE_URL;
 
