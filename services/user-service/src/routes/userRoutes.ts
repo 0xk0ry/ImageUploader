1ff8d477f5d4d90eb79545f3b236b6eec380aa38
kory <phamtruc.main@gmail.com>
2026-02-09 22:06:14 +0700

    refractor(user-service): rename controller to controllers

--- services/user-service/src/routes/userRoutes.ts
+++ services/user-service/src/routes/userRoutes.ts
@@ -1,6 +1,6 @@
 import { Router } from 'express';
-import { register, login } from '../controller/authController.js';
-import { getProfile, updateMyProfile   } from '../controller/userController.js';
+import { register, login } from '../controllers/authController.js';
+import { getProfile, updateMyProfile   } from '../controllers/userController.js';
 import { authenticate } from '../middleware/authMiddleware.js';
 
 const router = Router();
